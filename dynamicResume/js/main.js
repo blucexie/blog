d9d5ffc13da51dd85e154c3425b01dd0e7b68262
谢玉琪 <649453836@qq.com>
2018-06-28 15:52:42 +0800

    会动的简历完成

--- dynamicResume/js/main.js
+++ dynamicResume/js/main.js
@@ -1,37 +1,35 @@
-!function(){
-    let container = document.querySelector('#code');
+! function () {
+    function writeCode(prefix, code, fn) {
+        let container = document.querySelector('#code');
         let styleTag = document.querySelector('#styleTag');
-    function writeCode(code) {
-        
+        prefix.innerHTML = prefix || '';
         let n = 0;
-        let timer = setInterval(()=>{
+        let timer = setInterval(() => {
             n += 1;
-            container.innerHTML = code.substring(0, n);
-            styleTag.innerHTML = code.substring(0, n);
-            container.innerHTML =  Prism.highlight(container.innerHTML, Prism.languages.css);
+            container.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+            styleTag.innerHTML = prefix + code.substring(0, n);
             container.scrollTop = container.scrollHeight;
             if (n >= code.length) {
-               window.clearInterval(timer);
-               createPaper();
-               fn3(code);
+                window.clearInterval(timer);
+                fn.call()
             }
-        },10)
-        /*  let timer; 
-         timer=  setTimeout(function run() {
-            n += 1;
-            container.innerHTML = css1.substring(0, n);
-            styleTag.innerHTML = css1.substring(0, n);
-            container.innerHTML =  Prism.highlight(container.innerHTML, Prism.languages.css);
-            
-            container.scrollTop = container.scrollHeight;
-            if (n < css1.length) {
-                timer= setTimeout(run,5)
-            }else{
-                createPaper()
-            }
-            }, 5); */
-
+        }, 0)
     }
+
+    function writeMarkdown(markdown, fn) {
+        let dompaper = document.querySelector('.content');
+        let n = 0;
+        let timer = setInterval(() => {
+            n += 1;
+            dompaper.innerHTML = markdown.substring(0, n);
+            dompaper.scrollTop = dompaper.scrollHeight;
+            if (n >= markdown.length) {
+                window.clearInterval(timer);
+                fn.call()
+            }
+        }, 0)
+    }
+
     let code = `/* 
     * 面试官你好，我是XXX
     * 只用文字作做我介绍太单调了
@@ -61,50 +59,139 @@
     animation: breath 0.5s infinite alternate-reverse;
     }
     /* 加3D效果 */
-    #code{
-        transform:rotate(360deg)
+   
+    .code-wrapper{
+        perspective: 3000px;
+        position: fixed; 
+        left: 0; 
+        height: 100%;
+        width: 50%; 
     }
-    /* 现在正式开始 */
+    #code{
+        transform:rotateY(20deg)
+    }
 
     /* 我需要一张白纸 */
-
-    #code-wrapper{
-    width: 50%; left: 0; position: fixed; 
-    height: 100%;
-    }
-
+    
     #paper > .content {
-    display: block;
+        display: block;
     }
-
+   
     /* 于是我就可以在白纸上写字了，请看右边 */
     `
-    writeCode(code) 
+    var result = `
+    /* 接下来用一个优秀的库 marked.js
+     * 把 Markdown 变成 HTML
+     */`
+    var md = `
+# 自我介绍
 
-    function createPaper(){
+我叫 XXX
+1990 年 1 月出生
+XXX 学校毕业
+自学前端半年
+希望应聘前端开发岗位
+
+## 技能介绍
+
+熟悉 JavaScript CSS
+
+## 项目介绍
+
+1. XXX 轮播
+2. XXX 简历
+3. XXX 画板
+
+## 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式
+
+- QQ xxxxxxxx
+- Email xxxxxxxx
+- 手机 xxxxxxx# 联系方式`
+  var end = `  
+   
+    /*
+    * 这就是我的会动的简历
+    * 谢谢观看
+    */
+   `
+    writeCode('', code, () => {
+        createPaper(() => {
+            writeMarkdown(md, () => {
+                writeCode(code, result, () => {
+                    convertMarkdownToHtml(() => {
+                        writeCode(code+ result,end,()=>{
+                            console.log('end')
+                        })
+                    })
+                })
+            })
+        });
+    });
+
+    function createPaper(fn) {
         let paper = document.createElement('div');
         paper.id = 'paper';
         let content = document.createElement('pre');
-        content.className ='content'
-        paper.appendChild(content); 
+        content.className = 'content'
+        paper.appendChild(content);
         document.body.appendChild(paper);
+        fn.call();
     }
-    function fn3(){
-        var result = `#paper{
-            position:flxed;
-            width:50%;
-            background:red;
-        }`  
-        
-        let n = 0;
-        let timer = setInterval(()=>{
-            n += 1;
-            container.innerHTML = container.innerHTML+result.substring(n-1, n);
-            styleTag.innerHTML = result.substring(n-1, n);
-           console.log(result.substring(n-1,n));
-           if(n>result.length){
-               window.clearInterval(timer)
-           }
-        },10)
+
+    function convertMarkdownToHtml(fn) {
+        var div = document.createElement('div')
+        div.className = 'html markdown-body'
+        div.innerHTML = marked(md);
+        let markdownContainer = document.querySelector('#paper > .content')
+        markdownContainer.replaceWith(div)
+        fn && fn.call()
     }
 }.call()
